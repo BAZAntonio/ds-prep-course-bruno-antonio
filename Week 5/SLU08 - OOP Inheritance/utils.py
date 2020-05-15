d72478fda877637488475342dc1addedb9f85ce3
BAZAntonio <bralezevantonio@gmail.com>
2020-05-15 15:28:17 +0100

    Deleted extra SLUs

--- Week 5/SLU08 - OOP Inheritance/utils.py
+++
@@ -1,8 +0,0 @@
-import datetime, random
-
-def get_timestamp():
-    return datetime.datetime.now().isoformat('|', 'seconds')
-
-
-def get_random_number(start: int, stop: int):
-    return random.randint(start, stop)
--- Week 7/SLU11 - Command Lines & Text Editor/snippet.py
+++
@@ -1,3 +0,0 @@
-print('Hello World !')
-
-# print("VSCode is a whole new game! Goodbye, World!")
